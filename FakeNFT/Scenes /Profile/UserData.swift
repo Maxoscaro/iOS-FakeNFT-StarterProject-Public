2dea2f5bc038575bd9e26008456bd56ad179661f
Vegicode <vadimegiazarov@mail.ru>
2025-08-06 22:40:11 +0300

    Вернул UserData и внес исправления в .md файлы

---
+++ FakeNFT/Scenes /Profile/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  FakeNFT
+//
+//  Created by Mac on 06.08.2025.
+//
+
+import SwiftUI
+
+struct UserData: Codable {
+    var name: String
+    var description: String
+    var link: String
+    var imageData: Data?
+}
